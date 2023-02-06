00a8690eb3ba98bdba0aeca72880777b47c32a35
kartik <kartikmakhloga20@gmail.com>
2023-02-06 17:30:02 +0530

    countdown using hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { useState } from "react";
 
 function App() {
+
+  //here count is a variable and setCount is function to change the count variable.
+  //useState(0) is declaring the count variable with 0.
+  //after changing the definition of variable using setCount function component re-render fully
+  
   let [count, setCount] = useState(0);
   return (
     <div>
